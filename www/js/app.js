fe89dfce3c0bca7c31fde11116d8c2253fe5d1ee
addisonkaufmann <addisonkaufmann@email.arizona.edu>
2019-02-23 15:35:04 -0800

    basic page structure

--- www/js/app.js
+++ www/js/app.js
@@ -10,8 +10,59 @@ app.config(function($stateProvider, $urlRouterProvider) {
 
         .state('home', {
             url: '/home',
-            templateUrl: 'templates/partial-home.html',
+            templateUrl: 'templates/home.html',
             controller: 'homeController'
+        })
+
+        .state('art', {
+            url: '/art/:id',
+            templateUrl: 'templates/detail.html',
+            controller: 'detailController',
+            params: {
+                id: 'thesis',
+                navLinks: [
+                    {id: 'thesis', name: 'Thesis'},
+                    {id: 'ripple-effect', name: 'Ripple Effect'},
+                    {id: 'apply-flux', name: 'Apply Flux to Exclude Oxygen'},
+                    {id: 'body-without-organs', name: 'The Body without Organs'},
+                    {id: 'lost-in-translation', name: 'Lost in Translation'},
+                    {id: 'animus', name: 'Animus'},
+                    {id: 'speak', name: 'SPEAK'},
+                    {id: 'cryptic', name: 'Cryptic, Embedded'},
+                    {id: 'change-skins', name: 'I Want to Change Skins'},
+                ]
+            }
+        })
+
+        .state('social-practice', {
+            url: '/social-practice/:id',
+            templateUrl: 'templates/detail.html',
+            controller: 'detailController',
+            params: {
+                id: 'project-harvest',
+                navLinks: [
+                    {id: 'project-harvest', name: 'Project Harvest'},
+                    {id: 'solar-commons', name: 'Solar Commons'},
+                    {id: 'gardenroots', name: 'Gardenroots'},
+                    {id: 'activism', name: 'Activism'},
+                    {id: 'design', name: 'Design'},
+                    {id: 'video', name: 'Video'},
+                ]
+            }
+        })
+
+        .state('bio', {
+            url: '/bio/:id',
+            templateUrl: 'templates/detail.html',
+            controller: 'detailController',
+            params: {
+                id: 'bio',
+                navLinks: [
+                    {id: 'bio', name: 'Bio'},
+                    {id: 'cv', name: 'C. V.'},
+                    {id: 'contact', name: 'Contact'},
+                ]
+            }
         });
         // .state('sonic-data', {
         //     url: '/sonic-data',
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,3 +1,10 @@
 app.controller('homeController', ['$scope', '$window', '$sce', '$state', '$stateParams', '$timeout', function($scope, $window, $sce, $state, $stateParams, $timeout){
     console.log("home");
 }]);
+
+app.controller('detailController', ['$scope', '$window', '$sce', '$state', '$stateParams', '$timeout', function($scope, $window, $sce, $state, $stateParams, $timeout){
+    $scope.links = $stateParams.navLinks;
+    $scope.id = $stateParams.id;
+    $scope.stateName = $state.current.name;
+}]);
+
